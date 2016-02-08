dcb9b2da1373d9a990964667ed1e28bfebc37f33
Anatoli Angelov <anatoli.angelov@hotmail.com>
2016-02-08 16:05:50 +0200

    Finished 03 problem.

---
+++ 05-ASP.NET-Data-Binding/Data-Binding-App/XmlTreeView.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="XmlTreeView.aspx.cs" Inherits="Data_Binding_App.XmlTreeView" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:XmlDataSource ID="xmlFoodMenu" runat="server" DataFile="~/App_Data/food-menu.xml"></asp:XmlDataSource>
+    <asp:TreeView ID="treeViewXmlFoodMenu" runat="server"
+        DataSourceID="xmlFoodMenu"
+        OnSelectedNodeChanged="treeViewXmlFoodMenu_SelectedNodeChanged">
+        <DataBindings>
+            <asp:TreeNodeBinding DataMember="breakfast_menu" Text="Breakfast menu" />
+            <asp:TreeNodeBinding DataMember="food" TextField="name" />
+            <asp:TreeNodeBinding DataMember="price" TextField="#InnerText" />
+            <asp:TreeNodeBinding DataMember="description" TextField="#InnerText" />
+            <asp:TreeNodeBinding DataMember="calories" TextField="#InnerText" />
+        </DataBindings>
+    </asp:TreeView>
+    <asp:Label ID="SelectedNodeInnerXml" runat="server"></asp:Label>
+</asp:Content>
---
+++ 05-ASP.NET-Data-Binding/Data-Binding-App/XmlTreeView.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace Data_Binding_App
+{
+    public partial class XmlTreeView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void treeViewXmlFoodMenu_SelectedNodeChanged(object sender, EventArgs e)
+        {
+            TreeView tr = sender as TreeView;
+            TreeNodeCollection childNodes = tr.SelectedNode.ChildNodes;
+
+            string innerXml = GetInnerXml(childNodes, 0);
+
+            this.SelectedNodeInnerXml.Text = innerXml;
+        }
+
+        private static string GetInnerXml(TreeNodeCollection childNodes, int offSet)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            string offsetStr = new string(' ', offSet);
+            foreach (TreeNode childNode in childNodes)
+            {
+                strBuilder.Append(offsetStr + childNode.Text + "<br />");
+                strBuilder.Append(GetInnerXml(childNode.ChildNodes, offSet + 2));
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
